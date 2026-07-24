337e723401659ef251d55d737e992d3a4b6e0e38
laxmanyelle350 <laxmanyelle71@gmail.com>
2026-07-24 11:28:54 +0530

    modified

--- ass22.py
+++ ass22.py
@@ -1,3 +1,11 @@
-num="laxman"
-for i in num:
-    print(i)
+# num="laxman"
+# for i in num:
+#     print(i)
+
+
+rows=5
+for i in range(1,rows+1):
+    res=""
+    for j in range(1,rows):
+        res="*"
+    print(res)
